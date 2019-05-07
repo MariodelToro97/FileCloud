87e1ffb4d7553376b408398f2288407d74cce3da
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2019-05-06 20:46:09 -0500

    Inserción de datos
    Se insertan los datos dentro de Firebase

--- FileCloud Android/app/src/main/java/com/example/filecloud/Registro.java
+++ FileCloud Android/app/src/main/java/com/example/filecloud/Registro.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Registro extends AppCompatActivity {
 
@@ -14,7 +21,12 @@ public class Registro extends AppCompatActivity {
     private Button btnRegistro;
     private RadioButton rbNuevoIngreso;
     private RadioButton rbAlumnoInscrito;
-    private EditText ptNumeroMatricula;
+    private EditText editNumeroMatricula;
+    private EditText User;
+    private EditText nombre, apellidoPaterno, apelllidoMaterno, contrasena, confirmContrasena;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +37,57 @@ public class Registro extends AppCompatActivity {
         btnRegistro = findViewById(R.id.registrarse);
         rbAlumnoInscrito = findViewById(R.id.RB_alumnoInscrito);
         rbNuevoIngreso = findViewById(R.id.RB_nuevoIngreso);
-        ptNumeroMatricula = findViewById(R.id.PT_numeroMatricula);
+
+        editNumeroMatricula = findViewById(R.id.PT_numeroMatricula);
+        nombre = findViewById(R.id.nombreText);
+        apellidoPaterno = findViewById(R.id.paternoText);
+        apelllidoMaterno = findViewById(R.id.maternoText);
+        contrasena = findViewById(R.id.contrasenaText);
+        confirmContrasena = findViewById(R.id.confirmContrasenaText);
+        User = findViewById(R.id.userText);
 
         btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent registro = new Intent(Registro.this, inicioSesion.class);
-                startActivity(registro);
-                finish();
+
+                myRef = database.getReference("Users/"+User.getText().toString()+"/Usuario");
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+
+                        if (!value.equalsIgnoreCase(User.getText().toString())) {
+                            if (contrasena.getText().toString().equals(confirmContrasena.getText().toString())) {
+                                // Write a message to the database
+                                String reference = "Users/" + User.getText().toString();
+                                myRef = database.getReference(reference + "/Nombre");
+                                myRef.setValue(nombre.getText().toString());
+                                myRef = database.getReference(reference + "/apellidoPaterno");
+                                myRef.setValue(apellidoPaterno.getText().toString());
+                                myRef = database.getReference(reference + "/apellidoMaterno");
+                                myRef.setValue(apelllidoMaterno.getText().toString());
+                                myRef = database.getReference(reference + "/Password");
+                                myRef.setValue(contrasena.getText().toString());
+                                myRef = database.getReference(reference + "/Usuario");
+                                myRef.setValue(User.getText().toString());
+                                Intent registro = new Intent(Registro.this, inicioSesion.class);
+                                startActivity(registro);
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), R.string.noCoincidden, Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), R.string.userExist, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Toast.makeText(getApplicationContext(),R.string.errorBD,Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
